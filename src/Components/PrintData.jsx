e26c00a645c082e49cb949375b23b8f3e144b855
HumanshiJangra <Humanshijangra123@gmail.com>
2024-05-27 10:17:03 +0530

    27-05-24

--- src/Components/PrintData.jsx
+++ src/Components/PrintData.jsx
@@ -25,12 +25,13 @@ export default function PrintData() {
   };
   return (
     <div className="container mx-auto px-5">
-       <p className="pb-[40px]">{formData.email}</p>
+      <p className="ml-[-20px] pb-[40px]">{formData.email}</p>
       {formData ? (
         <div id="pdf-content" className="text-[#888989] px-12 main relative">
-         
           <div className="absolute w-[40%] p-5 bg-[#FFD402] top-0 left-0 ">
-            <p className=" text-white text-sm text-center">HIGH-TECH-MADE IN GERMANY</p>
+            <p className=" text-white text-sm text-center">
+              HIGH-TECH-MADE IN GERMANY
+            </p>
           </div>
           <div className="pt-28 grid grid-cols-2 ">
             <div className="left">
@@ -49,101 +50,123 @@ export default function PrintData() {
             <div className="right text-right ">
               <img className="max-w-[200px] ml-auto " src={logo} alt="" />
               <h2 className="mt-4 font-bold">Tel.: 0621 48 488 0</h2>
-              <h2 className="font-bold">Mail: <a href="kontakt@laufer-zahntechnik.de">kontakt@laufer-zahntechnik.de</a></h2>
+              <h2 className="font-bold">
+                Mail:{" "}
+                <a href="kontakt@laufer-zahntechnik.de">
+                  kontakt@laufer-zahntechnik.de
+                </a>
+              </h2>
             </div>
           </div>
-          <div className="flex justify-between mb-4">
+          <div className="flex justify-between my-[50px]">
             <p className="w-[60%]">
               Patient:............................................................................
             </p>
-            <p className="">Datum:............................................
-         
+            <p className="">
+              Datum:............................................
             </p>
           </div>
-          <div className="checkbox-form grid grid-cols-2 gap-5">
+          <div className="checkbox-form grid grid-cols-2 gap-[50px]">
             <div className="left justify-between flex gap-4">
               <div className="w-[30%]">
-              <div className="checkboxes">
-              <label>
-                EXCLUSIVE <br/>(BEL/Kasse)
-                {formData.pricerange === "ExclusivBELKASSE" && (
-                <input type="checkbox" name="ExclusivBELKASSE" 
-                checked={formData.pricerange === "ExclusivBELKASSE"}
-                id="" />
-                )}
-              </label>
-              
+                <div className="checkboxes">
+                  <label>
+                    EXCLUSIVE <br />
+                    (BEL/Kasse)
+                    {formData.pricerange === "ExclusivBELKASSE" && (
+                      <input
+                        type="checkbox"
+                        name="ExclusivBELKASSE"
+                        checked={formData.pricerange === "ExclusivBELKASSE"}
+                        id=""
+                      />
+                    )}
+                    <span className="checkmark"></span>
+                  </label>
+                </div>
+                <div className="checkboxes">
+                  <label>
+                    Preisgünstig/ <br />
+                    Laufer Zahntechnik
+                    {formData.pricerange === "Preisgünstig" && (
+                      <input
+                        type="checkbox"
+                        name="Preisgünstig"
+                        checked={formData.pricerange === "Preisgünstig"}
+                        id=""
+                      />
+                    )}
+                    <span className="checkmark"></span>
+                  </label>
+                </div>
               </div>
-              <div className="checkboxes">
-              <label>
-              Preisgünstig/ <br/>Laufer Zahntechnik
-                {formData.pricerange === "Preisgünstig" && (
-                <input type="checkbox" name="Preisgünstig" 
-                checked={formData.pricerange === "Preisgünstig"}
-                id="" />
-                )}
-              </label>
-              </div>
-              </div>
-              <div>
+              <div className="w-[50%]">
                 <h2>Krankenkasse</h2>
-                  <p className="border-solid border-[#878282] border-b-2 ">{formData.health}</p>
+                <p className="border-solid border-[#878282] border-b-2 ">
+                  {formData.health}
+                </p>
                 <h2>Implantatsystem</h2>
                 <p>{formData.implant}</p>
               </div>
             </div>
             <div className="right">
-              <p>Material:
-              {/* {formData.material} */}
+              <p>
+                Material:
+                {/* {formData.material} */}
               </p>
-              <div className="grid grid-cols-2">
+              <div className="custom-check-wrap grid grid-cols-2">
                 <div className="checkboxes">
-              <label className="">
-              Edelmetall:   </label>
-              {formData.material === "edelmetall" && (
-                <input
-                  type="checkbox"
-                  name="edelmetall"
-                  checked={formData.material === "edelmetall"}
-                />
-              )}
-              </div>
-            <div className="checkboxes">
-              <label className="">
-                NEM:
-                </label>
-                {formData.material === "nem" && (
-                <input
-                  type="checkbox"
-                  name="otherOption"
-                  checked={formData.material === "otherOption"}
-                />
-              )}
-           </div>
-           <div className="checkboxes">
-              <label className="">
-              Presskeramik:
-              </label>
-              {formData.material === "Presskeramik" && (
-                <input
-                  type="checkbox"
-                  name="Presskeramik"
-                  checked={formData.material === "Presskeramik"}
-                />
-              )}
-             </div>
-             <div className="checkboxes">
-              <label className="">
-              Zirkon:
-              </label>
-              {formData.material === "zirkon" && (
-                <input
-                  type="checkbox"
-                  name="zirkon"
-                  checked={formData.material === "zirkon"}
-                />
-              )}
-              </div>
+                  <label className="">
+                    Edelmetall:
+                    {formData.material === "edelmetall" && (
+                      <input
+                        type="checkbox"
+                        name="edelmetall"
+                        checked={formData.material === "edelmetall"}
+                      />
+                    )}
+                    <span className="checkmark"></span>
+                  </label>
+                </div>
+                <div className="checkboxes">
+                  <label className="">
+                    NEM:
+                    {formData.material === "nem" && (
+                      <input
+                        type="checkbox"
+                        name="otherOption"
+                        checked={formData.material === "otherOption"}
+                      />
+                    )}
+                    <span className="checkmark"></span>
+                  </label>
+                </div>
+                <div className="checkboxes">
+                  <label className="">
+                    Presskeramik:
+                    {formData.material === "Presskeramik" && (
+                      <input
+                        type="checkbox"
+                        name="Presskeramik"
+                        checked={formData.material === "Presskeramik"}
+                      />
+                    )}
+                    <span className="checkmark"></span>
+                  </label>
+                </div>
+                <div className="checkboxes">
+                  <label className="">
+                    Zirkon:
+                    {formData.material === "zirkon" && (
+                      <input
+                        type="checkbox"
+                        name="zirkon"
+                        checked={formData.material === "zirkon"}
+                      />
+                    )}
+                    <span className="checkmark"></span>
+                  </label>
+                </div>
               </div>
               <div>
                 <p></p>
@@ -151,9 +174,8 @@ export default function PrintData() {
             </div>
           </div>
 
-<TableNew tableData={formData.tableData} isPrintMode={true} />
+          <TableNew tableData={formData.tableData} isPrintMode={true} />
 
-         
           <Description />
           <textarea
             className="h-[200px] border-solid border-[grey] border-[1px] w-full p-2"
@@ -170,13 +192,13 @@ export default function PrintData() {
         <p>No data available</p>
       )}
       <div className="text-right px-12">
-      <button
-        onClick={downloadPdf}
-        className="button margin-left"
-        type="submit"
-      >
-        PDF Herunterladen
-      </button>
+        <button
+          onClick={downloadPdf}
+          className="button margin-left"
+          type="submit"
+        >
+          PDF Herunterladen
+        </button>
       </div>
     </div>
   );
